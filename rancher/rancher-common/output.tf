6597e4debebe42c5a2e400885b4dff5dab981c27
piseganesh123 <piseganesh123@gmail.com>
2023-08-08 10:45:06 +0000

    commented for old workload

--- rancher/rancher-common/output.tf
+++ rancher/rancher-common/output.tf
@@ -9,10 +9,10 @@ output "rancher_url" {
 #  description = "Docker command used to add a node to the quickstart cluster"
 #}
 
-output "custom_cluster_windows_command" {
-  value       = rancher2_cluster_v2.quickstart_workload.cluster_registration_token.0.insecure_windows_node_command
-  description = "Docker command used to add a windows node to the quickstart cluster"
-}
+#output "custom_cluster_windows_command" {
+#  value       = rancher2_cluster_v2.quickstart_workload.cluster_registration_token.0.insecure_windows_node_command
+#  description = "Docker command used to add a windows node to the quickstart cluster"
+#}
 
 output "learner_one_cluster_command" {
   value       = rancher2_cluster_v2.learner1_cluster.cluster_registration_token.0.insecure_node_command
