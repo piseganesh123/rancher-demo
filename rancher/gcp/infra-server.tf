3f9c33d7ff2e5a917a3b0973dfa8b2b99c7ec07d
piseganesh123 <piseganesh123@gmail.com>
2023-08-08 09:39:39 +0000

    updated name for learner one

--- rancher/gcp/infra-server.tf
+++ rancher/gcp/infra-server.tf
@@ -101,5 +101,5 @@ module "rancher_common" {
 
   workload_kubernetes_version = var.workload_kubernetes_version
   workload_cluster_name       = "quickstart-gcp-custom"
-  learner1_cluster_name       = "lrn1-cluster"
+  learner_one_cluster_name       = "lrn-one-cluster"
 }
