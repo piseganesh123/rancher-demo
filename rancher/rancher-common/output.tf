cd68c690e66e90be3b70ea51aa4cea7d8e234fa9
piseganesh123 <piseganesh123@gmail.com>
2023-08-08 08:34:57 +0000

    updated command name

--- rancher/rancher-common/output.tf
+++ rancher/rancher-common/output.tf
@@ -14,6 +14,11 @@ output "custom_cluster_windows_command" {
   description = "Docker command used to add a windows node to the quickstart cluster"
 }
 
+output "learner1_cluster_command" {
+  value       = rancher2_cluster_v2.learner1_cluster.cluster_registration_token.0.insecure_node_command
+  description = "Docker command used to add learner 1 node to the quickstart cluster"
+}
+
 output "learner2_custom_cluster_command" {
   value       = rancher2_cluster_v2.learner2_node.cluster_registration_token.0.insecure_node_command
   description = "Docker command used to add a node to the learner2 cluster"
