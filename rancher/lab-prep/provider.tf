5c516cc6ad5e8eadaf0eb287df7ea51d54dc0aba
piseganesh123 <piseganesh123@gmail.com>
2023-08-09 08:00:36 +0000

    updated provider in lab prep

--- rancher/lab-prep/provider.tf
+++ rancher/lab-prep/provider.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_providers {
+    rancher2 = {
+      source  = "rancher/rancher2"
+      version = "3.0.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+# Rancher2 bootstrapping provider
+provider "rancher2" {
+  alias = "bootstrap"
+
+  api_url  = "https://${var.rancher_server_dns}"
+  insecure = true
+  # ca_certs  = data.kubernetes_secret.rancher_cert.data["ca.crt"]
+  bootstrap = true
+}
+
 # Rancher2 administration provider
 provider "rancher2" {
   alias = "admin"
