07d97d8dc96a87950723119cc17b703676430ad4
piseganesh123 <piseganesh123@gmail.com>
2023-08-12 11:23:27 +0000

    updated values for lab

--- rancher/lab-prep/variables.tf
+++ rancher/lab-prep/variables.tf
@@ -8,3 +8,27 @@ variable "rancher_server_dns" {
   type        = string
   description = "DNS host name of the Rancher server"
 }
+
+variable "gcp_account_json" {
+  type        = string
+  description = "File path and name of service account access token file."
+  default     = "us-east4"
+
+}
+
+variable "gcp_project" {
+  type        = string
+  description = "GCP project in which the quickstart will be deployed."
+}
+
+variable "gcp_region" {
+  type        = string
+  description = "GCP region used for all resources."
+  default     = "us-east4"
+}
+
+variable "gcp_zone" {
+  type        = string
+  description = "GCP zone used for all resources."
+  default     = "us-east4-a"
+}
