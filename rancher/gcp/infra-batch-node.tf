87b521102e584cd5952a9d64c3351ed8495c9d67
piseganesh123 <piseganesh123@gmail.com>
2023-08-13 06:46:16 +0000

    updated batch node location

--- rancher/gcp/infra-batch-node.tf
+++ rancher/gcp/infra-batch-node.tf
@@ -1,21 +1,4 @@
 # GCP infrastructure resources
-
-resource "tls_private_key" "global_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-resource "local_sensitive_file" "ssh_private_key_pem" {
-  filename        = "${path.module}/id_rsa"
-  content         = tls_private_key.global_key.private_key_pem
-  file_permission = "0600"
-}
-
-resource "local_file" "ssh_public_key_openssh" {
-  filename = "${path.module}/id_rsa.pub"
-  content  = tls_private_key.global_key.public_key_openssh
-}
-
 # GCP Public Compute Address for quickstart node
 resource "google_compute_address" "learner2_node_address" {
 #  count = 2
@@ -86,28 +69,4 @@ resource "google_compute_instance" "learner2_node"{ #"quickstart_node"
 #      private_key = "../gcp/id_rsa"
     }
   }
-}
-
-module "rancher_common" {
-  source = "../rancher-common"
-
-#  node_public_ip             = google_compute_instance.rancher_server.network_interface.0.access_config.0.nat_ip
-#  node_internal_ip           = google_compute_instance.rancher_server.network_interface.0.network_ip
-#  node_username              = local.node_username
-#  ssh_private_key_pem        = tls_private_key.global_key.private_key_pem
-#  rancher_kubernetes_version = var.rancher_kubernetes_version
-
-#  cert_manager_version    = var.cert_manager_version
-#  rancher_version         = var.rancher_version
-#  rancher_helm_repository = var.rancher_helm_repository
-
-  rancher_server_dns = join(".", ["rancher", google_compute_instance.rancher_server.network_interface.0.access_config.0.nat_ip, "sslip.io"])
-  admin_password     = var.rancher_server_admin_password
-
-  workload_kubernetes_version = var.workload_kubernetes_version
-
- #worker node names 
-  workload_cluster_name       = "quickstart-gcp-custom"
-  learner1_cluster_name       = "learner1-cluster"
-  learner2_cluster_name       = "learner2-cluster"
 }
