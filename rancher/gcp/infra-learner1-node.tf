cd84d14b996a66e20fb78bb6c362e0d7cea42c97
piseganesh123 <piseganesh123@gmail.com>
2023-08-13 06:23:25 +0000

    updated username

--- rancher/gcp/infra-learner1-node.tf
+++ rancher/gcp/infra-learner1-node.tf
@@ -65,9 +65,9 @@ resource "google_compute_instance" "learner1_node"{ #"quickstart_node"
   }
 }
 
-#module "lab-prep" {
-#  source = "../lab-prep"
-#  gcp_project = "var.gcp_project"
-#  rancher_server_dns = join(".", ["rancher", google_compute_instance.rancher_server.network_interface.0.access_config.0.nat_ip, "sslip.io"]) 
-#  gcp_account_json = var.gcp_account_json
-#}
+module "lab-prep" {
+  source = "../lab-prep"
+  gcp_project = "var.gcp_project"
+  rancher_server_dns = join(".", ["rancher", google_compute_instance.rancher_server.network_interface.0.access_config.0.nat_ip, "sslip.io"]) 
+  gcp_account_json = var.gcp_account_json
+}
