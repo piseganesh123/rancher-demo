ea58d80cd23dca5e363f0fe59679209589e4b009
piseganesh123 <piseganesh123@gmail.com>
2023-08-13 09:53:03 +0000

    updated code

--- rancher/rancher-common/output.tf
+++ rancher/rancher-common/output.tf
@@ -20,6 +20,6 @@ output "learner_1_cluster_command" {
 }
 
 output "learner2_custom_cluster_command" {
-  value       = rancher2_cluster_v2.learner2_node.cluster_registration_token.0.insecure_node_command
+  value       = rancher2_cluster_v2.learner2_cluster.cluster_registration_token.0.insecure_node_command
   description = "Docker command used to add a node to the learner2 cluster"
 }
